250a8c898519e4ef1bc9388a672c740880c33529
Fatima Zahra ELkihel <fatimazehraelkihel@gmail.com>
2023-06-03 18:16:47 +0100

    4eme

--- Gestion_compte/admin.py
+++ Gestion_compte/admin.py
@@ -1,12 +1,7 @@
 from django.contrib import admin
 from . import models
-<<<<<<< HEAD
-=======
 
 admin.site.register(models.client)
 admin.site.register(models.adresse_livraison)
 admin.site.register(models.order)
 admin.site.register(models.orderItem)
->>>>>>> origin/master
-# Register your models here.
-admin.site.register(models.profile)
