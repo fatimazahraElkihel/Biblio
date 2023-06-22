0a687176289664a2fbb5c71d15b422b5c362ccf6
samrani <samraninisrine2002@gmail.com>
2023-06-22 03:13:00 +0100

    11

--- store/models.py
+++ store/models.py
@@ -18,6 +18,7 @@ class Product (models.Model):
     slug = models.SlugField(max_length=128)
     digital=models.BooleanField(default=False,null=True,blank=False)
     image=models.ImageField(null=True,blank=True)
+    #description=models.CharField(max_length=1000,null=True)
 
     def __str__(self):
         return self.name
